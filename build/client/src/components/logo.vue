456082bedfe4d37df82a6ee02564f4f6186778c9
Thomas Grantley-Davies <tgrantle@myune.edu.au>
2019-09-08 20:04:05 +1000

    Added dodgy animation and Fixed L

--- build/client/src/components/logo.vue
+++ build/client/src/components/logo.vue
@@ -9,19 +9,29 @@
 </template>
    
 <script>
-import T from '../../res/T.svg';
-import O from '../../res/O.svg';
-import N from '../../res/N.svg';
-import E from '../../res/3.svg';
-import L from '../../res/L.svg';
+import T from "../../res/T.svg";
+import O from "../../res/O.svg";
+import N from "../../res/N.svg";
+import E from "../../res/3.svg";
+import L from "../../res/L.svg";
+import anime from "animejs/lib/anime.es.js";
 
 export default {
-    components: {
-        T,
-        O,
-        N,
-        E,
-        L
-    }
-}
+  components: {
+    T,
+    O,
+    N,
+    E,
+    L
+  },
+  mounted() {
+    console.log("ANIMATING");
+    anime({
+      targets: ".logo-letter",
+      duration: 1000,
+      elasticity: 400,
+      width: 200
+    });
+  }
+};
 </script>
