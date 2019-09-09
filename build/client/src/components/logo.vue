6d2a11d5523181fa85c3cb47d14b83f32ccb5e91
Thomas Grantley-Davies <tgrantle@myune.edu.au>
2019-09-09 16:01:02 +1000

    Make logo sexy

--- build/client/src/components/logo.vue
+++ build/client/src/components/logo.vue
@@ -27,10 +27,43 @@ export default {
   mounted() {
     console.log("ANIMATING");
     anime({
-      targets: ".logo-letter",
-      duration: 1000,
-      elasticity: 400,
-      width: 200
+      targets: ".logo_path",
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: "easeInSine",
+      duration: 5500,
+      delay: anime.stagger(100)
+    });
+    anime({
+      targets: ".logo_t",
+      delay: 5500,
+      duration: 5500,
+      fill: ["rgba(0,0,0,0)", "#fefefe"]
+    });
+    anime({
+      targets: ".logo_o",
+      delay: 5600,
+      duration: 5500,
+      fill: ["rgba(0,0,0,0)", "#fefefe"]
+    });
+    anime({
+      targets: ".logo_n",
+      delay: 5700,
+      duration: 5500,
+      fill: ["rgba(0,0,0,0)", "#b2b2b2"]
+    });
+    anime({
+      targets: ".logo_3",
+      delay: 5800,
+      duration: 5500,
+      fill: ["rgba(0,0,0,0)", "#707070"],
+      stroke: ["#fefefe", "#707070"]
+    });
+    anime({
+      targets: ".logo_l",
+      delay: 5900,
+      duration: 5500,
+      fill: ["rgba(0,0,0,0)", "#232223"],
+      stroke: ["#fefefe", "#232223"]
     });
   }
 };
